3f59e7b693d0af68c85222d7874d2138e21a316f
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-10 14:28:07 -0400

    Loaded images from assets in settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+import pygame
+import os
+
+
 #Colors (r, g, b)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -19,3 +23,18 @@ WIDTH = TILESIZE * ROWS
 HEIGHT = TILESIZE * COLUMNS
 FPS = 60
 TITLE = "Minesweeper <3"
+
+#List holding all numbers
+tile_numbers = []
+#Numbers go from 1 to 8 - Image assets are Tile1.png - Tile8.png
+for i in range(1,9): 
+    #Using transform to scale this image to TILESIZE BY TILESIZE
+    #Using image loads and the os path join to join the assets folder and cakk the file name
+    tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Tile{i}.png")), (TILESIZE, TILESIZE)))
+
+tile_empty = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileEmpty.png")), (TILESIZE, TILESIZE))
+tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileExploded.png")), (TILESIZE, TILESIZE))
+tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileFlag.png")), (TILESIZE, TILESIZE))
+tile_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileMine.png")), (TILESIZE, TILESIZE))
+tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileUnknown.png")), (TILESIZE, TILESIZE))
+tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileNotMine.png")), (TILESIZE, TILESIZE))
