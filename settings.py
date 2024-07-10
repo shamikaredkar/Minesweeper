f493b3535554487269b2d0992bf7c93483af9aa9
Shamika Redkar <shamikaredkar91@gmail.com>
2024-07-10 13:57:24 -0400

    Added visual settings to settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+#Colors (r, g, b)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+DARKGREEN = (0, 200, 0)
+BLUE = (0 , 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BGCOLOR = DARKGREY
